7c82570a6a42213b163b9f8b3c7cb8a17b7618ca
user638726 <lienjackson35@gmail.com>
2025-02-07 14:29:23 +0800

    完成登入驗證碼功能

--- back.php
+++ back.php
@@ -1,3 +1,6 @@
+<?php include_once "./api/db.php";?>
+
+<db class="php"></db>
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0057)?do=admin -->

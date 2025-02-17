defd091ce09b52079aba4d5972befa9520cc3bd5
user638726 <lienjackson35@gmail.com>
2025-02-17 13:46:23 +0800

    購物車建立

--- front/buycart.php
+++ front/buycart.php
@@ -1,6 +1,15 @@
 <?php
+if(!isset($_SESSION['cart'])){
+    $_SESSION['cart']=[];
+}
 
-echo $_GET['id'];
-echo "<br>";
-echo $_GET['qt'];
+if(!empty($_GET['id'])){
+    $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+}
+
+if(!isset($_SESSION['Mem'])){
+ to("index.php?do=login");
+}
+
 ?>
+<h2 class="ct"><?=$_SESSION['Mem'];?>的購物車</h2>
